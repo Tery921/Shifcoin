c3cd162f5ffefb2e0da1d93370e1c22ef297dee1
Tery921 <84305317+Tery921@users.noreply.github.com>
2021-06-17 17:42:26 +0200

    Add files via upload

---
+++ src/shifcoin/Wallet.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shifcoin;
+import java.util.*;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *
+ * @author Java
+ */
+public class Wallet {
+
+    
+    public PrivateKey privateKey;
+	public PublicKey publicKey;
+	
+	public HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>(); //only UTXOs owned by this wallet.
+	
+	//public Wallet() {...
+		
+	//public void generateKeyPair() {...
+	
+  //returns balance and stores the UTXO's owned by this wallet in this.UTXOs
+	public float getBalance() {
+		float total = 0;	
+        for (Map.Entry<String, TransactionOutput> item: Shifcoin.UTXOs.entrySet()){
+        	TransactionOutput UTXO = item.getValue();
+            if(UTXO.isMine(publicKey)) { //if output belongs to me ( if coins belong to me )
+            	UTXOs.put(UTXO.id,UTXO); //add it to our list of unspent transactions.
+            	total += UTXO.value ; 
+            }
+        }  
+		return total;
+	}
+	//Generates and returns a new transaction from this wallet.
+	public Transaction sendFunds(PublicKey _recipient,float value ) {
+		if(getBalance() < value) { //gather balance and check funds.
+			System.out.println("#Not Enough funds to send transaction. Transaction Discarded.");
+			return null;
+		}
+    //create array list of inputs
+		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+    
+		float total = 0;
+		for (Map.Entry<String, TransactionOutput> item: UTXOs.entrySet()){
+			TransactionOutput UTXO = item.getValue();
+			total += UTXO.value;
+			inputs.add(new TransactionInput(UTXO.id));
+			if(total > value) break;
+		}
+		
+		Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
+		newTransaction.generateSignature(privateKey);
+		
+		for(TransactionInput input: inputs){
+			UTXOs.remove(input.transactionOutputId);
+		}
+		return newTransaction;
+	}
+        
+        public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+		int count = transactions.size();
+		ArrayList<String> previousTreeLayer = new ArrayList<String>();
+		for(Transaction transaction : transactions) {
+			previousTreeLayer.add(transaction.transactionId);
+		}
+		ArrayList<String> treeLayer = previousTreeLayer;
+		while(count > 1) {
+			treeLayer = new ArrayList<String>();
+			for(int i=1; i < previousTreeLayer.size(); i++) {
+				treeLayer.add(StringUtil.applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+			}
+			count = treeLayer.size();
+			previousTreeLayer = treeLayer;
+		}
+		String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+		return merkleRoot;
+	}
+	
+}
